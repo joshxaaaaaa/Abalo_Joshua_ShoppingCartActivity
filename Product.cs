461b78ae1b08f8339417397e4519f2893869a2de
Abalo, Joshua <abalo.schoolpurposes@gmail.com>
2026-04-14 11:44:08 +0800

    Commit 1
    Product Class: 
    Since this is the first day of coding this project, I started on this class named Product and added their fields/properties. Also, with this class, I added methods such as displayProduct as stated on the requirement. 
    
    Main Class: 
    This Main class is the second class I created containing the arrays of the objects, constructors, and initial switch cases. Also, I assigned product names and the values of each property on this class. Lastly, I created a user dashboard to identify the all-possible user actions may occur. 
    
    AI Usage: 
    I spent my time using AI to explore this platform. Since this is my first semester using Github, I frequently asked AI anything about repositories and how it works. Also, not only about this platform, I asked AI about the requirement prompting, "How to use int.TryParse() and why is it better than Convert.ToInt32()?" to know its usage in this project. Lastly, my overall code on this project was from the previous discussions and online video tutorials on YT (specifically Bro Code), and I applied these learnings to create this project. 
    
    Any further improvement will be expected soon.

---
+++ Product.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JAShoppingCartSystem
+{
+    class Products
+    {
+        public int prodIds;
+        public string prodNames;
+        public double prodPrices;
+        public int prodStocks;
+        public Products (int prodIds, String prodNames, double prodPrices, int proStocks)
+        {
+            this.prodIds = prodIds;
+            this.prodNames = prodNames;
+            this.prodPrices = prodPrices;
+            this.prodStocks = proStocks;
+        }
+        public void displayProducts()
+        {
+            Console.WriteLine($"{prodIds,-7} {prodNames,-12} {prodPrices,10:F2}   {prodStocks,8}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Transactions;
+
+namespace JAShoppingCartSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Products product1 = new Products(001, "Laptop", 40000.00, 60); 
+            Products product2 = new Products(002, "Tablet", 30000.00, 60); 
+            Products product3 = new Products(003, "Smartphone", 20000.00, 60); 
+            Products product4 = new Products(004, "Earphone", 400.00, 80); 
+            Products product5 = new Products(005, "Headphone", 1000.00, 80); 
+            Products product6 = new Products(006, "Keyboard", 1000.00, 75); 
+            Products product7 = new Products(007, "Mouse", 500.00, 75); 
+            Products product8 = new Products(008, "Microphone", 3000.00, 50); 
+            Products product9 = new Products(009, "Printer", 30000.00, 30); 
+            Products product10 = new Products(010, "Fan Cooler", 2000.00, 90);
+
+            Products[] prods = new Products[10];
+            prods[0] = product1;
+            prods[1] = product2;
+            prods[2] = product3;
+            prods[3] = product4;
+            prods[4] = product5;
+            prods[5] = product6;
+            prods[6] = product7;
+            prods[7] = product8;
+            prods[8] = product9;
+            prods[9] = product10;
+
+            bool isHere = true;
+
+            while (isHere)
+            {
+                Console.WriteLine("Welcome to JA Gadgets & Accessories");
+                Console.WriteLine("1 - View Products");
+                Console.WriteLine("2 - Add to Cart");
+                Console.WriteLine("3 - Check Cart");
+                Console.WriteLine("4 - Payment");
+                Console.WriteLine("5 - Exit");
+                Console.Write("Select a number ONLY: ");
+                string user = Console.ReadLine();
+                int choice;
+
+                if (!int.TryParse(user, out choice))
+                {
+                    Console.WriteLine("Non-numeric numbers are not acceptable");
+                }
+
+                if (choice > 5 || choice < 1)
+                {
+                    Console.WriteLine("Select from number 1 to 5 ONLY!");
+                }
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("PRODUCTS");
+                        Console.WriteLine("IDs |     Names     |   Prices   |   Stocks");
+                        foreach (var product in prods)
+                        {
+                            product.displayProducts();
+                        }
+                        break;
+
+                    case 5:
+                        Console.WriteLine("Thanks for visiting!");
+                        isHere = false;
+                        break;
+                        
+                }
+            }
+        }
+    }
+}       
